677c4d5b8246d1b330fbfd8a1dbbb6c26b63d25e
Md. Jalal Uddin Chowdhury <JalalChowdhuryCSE1999@gmail.com>
2021-11-10 23:46:54 +0600

    done

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 
 app.use(cors());
+app.use(express.json());
 
 
 
@@ -17,16 +18,81 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 // console.log(uri);
 
-async function run(){
+async function run() {
 
-    try{
-        await client.connect();
-        console.log("DataBase connected successfully");
+  try {
+    await client.connect();
+    const database = client.db('doctors_portal');
+    const appointmentsCollection = database.collection('appointments');
+    const usersCollection = database.collection('users');
 
-    }
-    finally{
-        // await client.close();
-    }
+    // for appoint
+    app.get('/appointments', async (req, res) => {
+      const email = req.query.email;
+      const date = new Date(req.query.date).toLocaleDateString();
+
+      const query = { email: email, date: date }
+
+      const cursor = appointmentsCollection.find(query);
+      const appointments = await cursor.toArray();
+      res.json(appointments);
+    })
+
+    // get user email for admin
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === 'admin') {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    })
+
+
+    // appointment store
+    app.post('/appointments', async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentsCollection.insertOne(appointment);
+      console.log(result);
+      res.json(result)
+    });
+
+    //for users
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+
+    app.put('/users', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+
+    // admin 
+    app.put('/users/admin', async (req, res) => {
+      const user = req.body;
+      console.log('put', user);
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: 'admin' } };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+
+    })
+
+
+  }
+  finally {
+    // await client.close();
+  }
 
 }
 run().catch(console.dir);
